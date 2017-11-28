af61bf0ffd801dc70adc61f22a76bba66c30fd97
Rik van den Berg <wfrvdb@gmail.com>
2017-11-28 10:57:34 +0100

    demo

---
+++ src/Hr.Assignment/WordCounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hr.Assignment
+{
+    public class WordCounter
+    {
+        private readonly IWordSplitter _wordSplitter;
+
+        public WordCounter(IWordSplitter wordSplitter)
+        {
+            _wordSplitter = wordSplitter;
+        }
+
+        public Dictionary<string, int> CountWords(string input)
+        {
+            string[] words = _wordSplitter.Split(input);
+            words = _wordSplitter.Split(input);
+            words = _wordSplitter.Split(input);
+            var result = words.Distinct().ToDictionary(w => w, w => 0);
+            for(int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                result[word]++;
+            }
+
+            return result;
+        }
+    }
+
+    public interface IWordSplitter
+    {
+        string[] Split(string input);
+    }
+}
--- test/Hr.Assignment.Tests/GivenATest_WhenExecuting_ItSucceeds.cs
+++ test/Hr.Assignment.Tests/GivenATest_WhenExecuting_ItSucceeds.cs
@@ -1,15 +1,104 @@
-﻿using Xunit;
+﻿using Moq;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Hr.Assignment.Tests
 {
     public class WhenExecutingAXUnitTest
     {
         [Fact]
-        public void GivenABooleanTrue_ThenItIsTrue()
+        public void GivenASampleSentence_Then_WeExpectTheCorrectOutput()
         {
-            var value = true;
+            // Arrange
+            var systemUnderTest = new WordCounter(new WordSplitterStub());
+            string sampleSentence = "This is the sample input for the application";
 
-            Assert.Equal(value, true);
-        } 
+            // Act
+            Dictionary<string, int> output = systemUnderTest.CountWords(sampleSentence);
+
+            // Assert
+            Assert.Equal(output["the"], 2);
+            Assert.Equal(output["is"], 1);
+            Assert.Equal(output["for"], 1);
+            Assert.Equal(output["this"], 1);
+            Assert.Equal(output["sample"], 1);
+            Assert.Equal(output["application"], 1);
+        }
+
+        [Fact]
+        public void GivenASampleSentence_Then_WeExpectTheCorrectOutput2()
+        {
+            // Arrange
+            var systemUnderTest = new WordCounter(new WordSplitterStub());
+            string sampleSentence = "This|is|the|sample|input|for|the|application";
+
+            // Act
+            Dictionary<string, int> output = systemUnderTest.CountWords(sampleSentence);
+
+            // Assert
+            Assert.Equal(output["the"], 2);
+            Assert.Equal(output["is"], 1);
+            Assert.Equal(output["for"], 1);
+            Assert.Equal(output["this"], 1);
+            Assert.Equal(output["sample"], 1);
+            Assert.Equal(output["application"], 1);
+        }
+
+        [Fact]
+        public void GivenASampleSentence_Then_WeExpectTheCorrectOutput3()
+        {
+            // Arrange
+            var systemUnderTest = new WordCounter(new WordSplitterStub());
+            string sampleSentence = "This|is|the|sample|input|for|the|application";
+
+            // Act
+            Dictionary<string, int> output = systemUnderTest.CountWords(sampleSentence);
+
+            // Assert
+            Assert.Equal(output["the"], 2);
+            Assert.Equal(output["is"], 1);
+            Assert.Equal(output["for"], 1);
+            Assert.Equal(output["this"], 1);
+            Assert.Equal(output["sample"], 1);
+            Assert.Equal(output["application"], 1);
+        }
+
+        [Fact]
+        public void When_Mocking()
+        {
+            // Arrange
+            //MOQ
+            var mockSplitter = new Mock<IWordSplitter>(MockBehavior.Strict);
+            var systemUnderTest = new WordCounter(mockSplitter.Object);
+            string sampleSentence = "This|is|the|sample|input|for|the|application";
+            mockSplitter.Setup(splitter => splitter.Split(sampleSentence))
+                .Returns(new string[] { "application" });
+
+            // Act
+            Dictionary<string, int> output = systemUnderTest.CountWords(sampleSentence);
+
+            // Assert
+            mockSplitter.VerifyAll();
+            mockSplitter.Verify(splitter => splitter.Split(sampleSentence), Times.Exactly(3));
+            Assert.Equal(output["application"], 1);
+
+        }
+
+        public class WordSplitterStub : IWordSplitter
+        {
+            public string[] Split(string input)
+            {
+                return new [] {
+                    "this",
+                    "is",
+                    "the",
+                    "sample",
+                    "input",
+                    "for",
+                    "the",
+                    "application",
+                };
+            }
+        }
     }
 }
