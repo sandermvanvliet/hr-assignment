8ef9dfb08b1734fca912f9cc0f28e63ad3a6f26a
Sander van Vliet <s.vanvliet@coolblue.nl>
2017-11-28 14:50:01 +0100

    Implementation example

---
+++ test/Hr.Assignment.Tests/CountingWords.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Hr.Assignment.Tests
+{
+    public class CountingWords
+    {
+        [Fact]
+        public void GivenAScentence_AListOfWordsIsReturned()
+        {
+            // Given
+            var scentence = "This is the sample input for the application";
+
+            // Act
+            var listOfWords = CountWords(scentence);
+
+            // Assert
+            listOfWords
+                .Should()
+                .NotBeEmpty();
+        }
+
+        [Fact]
+        public void GivenAScentence_TheWordTheIsCountedTwice()
+        {
+            // Given
+            var scentence = "This is the sample input for the application";
+
+            // Act
+            var listOfWords = CountWords(scentence);
+
+            // Assert
+            listOfWords["the"]
+                .Should()
+                .Be(2);
+        }
+
+        private static Dictionary<string, int> CountWords(string scentence)
+        {
+            var words = scentence.Split(' ');
+
+            var count = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (count.ContainsKey(word))
+                {
+                    count[word] += 1;
+                }
+                else
+                {
+                    count.Add(word, 1);
+                }
+            }
+
+            return count;
+        }
+    }
+}
--- test/Hr.Assignment.Tests/GivenATest_WhenExecuting_ItSucceeds.cs
+++ test/Hr.Assignment.Tests/GivenATest_WhenExecuting_ItSucceeds.cs
@@ -1,15 +1,56 @@
-﻿using Xunit;
+﻿//using System.Collections.Generic;
+//using FluentAssertions;
+//using Xunit;
 
-namespace Hr.Assignment.Tests
-{
-    public class WhenExecutingAXUnitTest
-    {
-        [Fact]
-        public void GivenABooleanTrue_ThenItIsTrue()
-        {
-            var value = true;
+//namespace Hr.Assignment.Tests
+//{
+//    public class WhenExecutingAXUnitTest
+//    {
+//        [Fact]
+//        public void GivenAScentence_AListOfCountsIsReturned()
+//        {
+//            var input = "This is the sample input for the application";
 
-            Assert.Equal(value, true);
-        } 
-    }
-}
+//            // Do something here
+//            var listOfCounts = CountWordsIn(input);
+
+//            listOfCounts
+//                .Should()
+//                .NotBeEmpty();
+//        }
+
+//        [Fact]
+//        public void GivenAScentence_TheWordTheIsCountedTwice()
+//        {
+//            var input = "This is the sample input for the application";
+
+//            // Do something here
+//            var listOfCounts = CountWordsIn(input);
+
+//            listOfCounts["the"]
+//                .Should()
+//                .Be(2);
+//        }
+
+//        private Dictionary<string, int> CountWordsIn(string input)
+//        {
+//            var countWordsIn = new Dictionary<string, int>();
+
+//            var words = input.Split(' ');
+
+//            foreach (var word in words)
+//            {
+//                if (countWordsIn.ContainsKey(word))
+//                {
+//                    countWordsIn[word] += 1;
+//                }
+//                else
+//                {
+//                    countWordsIn.Add(word, 1);
+//                }
+//            }
+            
+//            return countWordsIn;
+//        }
+//    }
+//}
